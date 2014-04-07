8f3bfa4ed7a1b0b301317cdf42f7203ea04f44cd
AbdulMuqsit <abdulmuqsitpakistani@yahoo.com>
2014-04-08 01:21:56 +0500

    Initial Commit

---
+++ PMS.App/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Windows;
+using PMS.DataAccess;
+
+namespace PMS.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DoctorRepository repository = new DoctorRepository();
+
+
+           lstBox.ItemsSource = repository.GetAll().ToList().Select(e=>e.name);
+        }
+    }
+}
---
+++ PMS.DataAccess/DoctorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+
+namespace PMS.DataAccess
+{
+    public class DoctorRepository
+    {
+        PmsEntities entities = new PmsEntities();
+
+        public IQueryable<doctor> GetAll()
+        {
+            return entities.doctors.AsQueryable();
+        }
+    }
+}
